f1c704f2b632a39014f0875b0a1dbfcd23933d7c
Alecksey Kiselev <Kiselev.Alecksey@yandex.ru>
2024-05-08 13:09:14 +0300

    add custom LinkedHashMap in InMemoryHistoryManager add class Node add new tests for historyManager

---
+++ src/model/Node.java
@@ -0,0 +1,38 @@
+package model;
+
+public final class Node<Task> {
+    Task value;
+    Node<Task> next;
+    Node<Task> prev;
+
+    public Node(Node<Task> prev, Task value, Node<Task> next) {
+        this.value = value;
+        this.next = next;
+        this.prev = prev;
+    }
+
+    public Task getValue() {
+        return value;
+    }
+
+    public void setValue(Task value) {
+        this.value = value;
+    }
+
+    public Node<Task> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<Task> next) {
+        this.next = next;
+    }
+
+    public Node<Task> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node<Task> prev) {
+        this.prev = prev;
+    }
+}
+
--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -1,34 +1,114 @@
 package service;
 
+import model.Node;
 import model.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager{
-    private static final int HISTORY_SIZE = 10;
-    private final List<Task> history = new ArrayList<>(HISTORY_SIZE);
+    private final LinkedListTasks<Task> linkedListTasks = new LinkedListTasks();
+    private final Map<Integer, Node<Task>> history = new HashMap<>();
+
 
     @Override
     public Task add(Task task) {
-        int sizeHistory = history.size();
-
-        if (history.contains(task)) {
-            history.remove(task);
+        if (task == null) {
+            return null;
         }
 
-        if (sizeHistory > 9) {
-            history.removeFirst();
+        Node<Task> node = history.get(task.getId());
+
+        if (node != null) {
+            linkedListTasks.removeNode(node);
         }
 
-        history.add(task);
-        return task;
+        linkedListTasks.linkLast(task);
+        history.put(task.getId(), linkedListTasks.last);
+        return new Task(task);
     }
 
     @Override
     public List<Task> getHistory() {
-        List<Task> list = new ArrayList<>(history);
-        return list;
+        return linkedListTasks.getTasks();
+    }
+
+    @Override
+    public int remove(int id) {
+        Node<Task> node = history.get(id);
+
+        if (node != null) {
+            linkedListTasks.removeNode(node);
+        }
+        history.remove(id);
+        return id;
+    }
+
+    private static class LinkedListTasks<Task> {
+
+        private int size = 0;
+        private Node<Task> first;
+        private Node<Task> last;
+
+        public LinkedListTasks() {
+        }
+
+        public void linkLast(Task task) {
+
+            final Node<Task> l = last;
+            final Node<Task> newNode = new Node<>(l, task, null);
+            last = newNode;
+            if (l == null)
+                first = newNode;
+            else
+                l.setNext(newNode);
+            size++;
+        }
+
+        public List<Task> getTasks() {
+            List<Task> list = new ArrayList<>();
+            Node<Task> node = first;
+
+            while(node != null) {
+                list.add(node.getValue());
+                node = node.getNext();
+            }
+
+            return list;
+        }
+
+        public Node<Task> removeNode(Node<Task> node) {
+            Node<Task> next = node.getNext();
+            Node<Task> prev = node.getPrev();
+
+            if ((next == null) && (prev == null)) {
+                first = null;
+                last = null;
+                size--;
+                return node;
+            }
+
+            if ((next != null) && (prev != null)) {
+                next.setPrev(prev);
+                prev.setNext(next);
+            }
+
+            if (prev == null) {
+                next.setPrev(null);
+                first = next;
+            }
+
+            if (next == null) {
+                prev.setNext(null);
+                last = prev;
+            }
+
+            size--;
+            return node;
+        }
+
     }
 
 }
